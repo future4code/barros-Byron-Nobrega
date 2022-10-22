cdf8ae281255d0beaba5afb27807adc0fd7842d8
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-10-22 12:19:58 -0300

    refactor se aplicado este commit ira refatorar exercico 9

--- modulo5/lista-typescript/src/exercicio9.ts
+++ modulo5/lista-typescript/src/exercicio9.ts
@@ -1,31 +1,53 @@
+const cpfInvalidos:Array<string>=[
+    "00000000000",
+    "11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666",
+    "77777777777",
+    "88888888888",
+    "99999999999",
+]
+
+
 function validarCpf(cpf: string):boolean {
   const numberCpf = cpf.replace(".","").replace(".","").replace("-","")
-  let soma = 0
-  let resto
-
-  for (let i = 1; i <= 9; i++) {
-    soma = soma + parseInt(numberCpf.substring(i-1,i))*(11-i) 
-  }
-  resto = (soma*10)%11; 
-  if((resto===10)||(resto===11)){
-    resto=0;
-  }
-  if (resto!=parseInt(numberCpf.substring(9,10))){
-    return false;
-  }
-  soma=0; 
-   for (let i = 1; i <=10; i++) {
-    soma = soma + parseInt(numberCpf.substring(i-1,i))*(12-i)
-  }
-  resto = (soma*10)%11
-  if (resto===10||resto===11) {
-    resto=0
-  }
-  if (resto!=parseInt(numberCpf.substring(10,11))){
-    return false;
-  } 
- 
-   return true
+  let soma:number = 0
+  let modulo:number
+    const cpfValido = cpfInvalidos.some((item)=>{
+       return item == numberCpf 
+    })
+  
+if(cpfValido!=true){
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(numberCpf.substring(i-1,i))*(11-i) 
+      }
+      modulo = (soma*10)%11; 
+      if((modulo===10)||(modulo===11)){
+        modulo=0;
+      }
+      if (modulo!=parseInt(numberCpf.substring(9,10))){
+        return false;
+      }
+      soma=0; 
+       for (let i = 1; i <=10; i++) {
+        soma += parseInt(numberCpf.substring(i-1,i))*(12-i)
+      }
+      modulo = (soma*10)%11
+      if (modulo===10||modulo===11) {
+        modulo=0
+      }
+      if (modulo!=parseInt(numberCpf.substring(10,11))){
+        return false;
+      } 
+     
+       return true
+}else{
+    return false    
 }
 
-console.log(validarCpf("728.760.680-10"));
+}
+
+console.log(validarCpf("295.563.566"));
