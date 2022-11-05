6f8eda1909e1875408221b0f5aad4fbb882debdf
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-11-04 22:01:00 -0300

    Feat: Se aplicado, este commit add os exercicíos apis-rest

---
+++ modulo5/apis-rest/src/respostas.js
@@ -0,0 +1,21 @@
+//Exercicío 1
+/*a. Qual método HTTP você deve utilizar para isso?
+Get 
+*/
+
+/*b. Como você indicaria a entidade que está sendo manipulada?
+
+*/
+
+
+//Exercício 2
+
+//Exercícios 3
+
+//Exercícios 4
+
+//Exercícios 5
+
+//Exercícios 6
+
+//Exercícios 7
