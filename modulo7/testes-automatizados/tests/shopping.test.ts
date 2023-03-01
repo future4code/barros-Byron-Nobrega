9899a98fe198c3295c40855d4adc42583fd48665
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2023-02-28 21:29:00 -0300

    Feat: se aplicado, este commit add os testes

--- modulo7/testes-automatizados/tests/shopping.test.ts
+++ modulo7/testes-automatizados/tests/shopping.test.ts
@@ -4,7 +4,7 @@ import { User } from "../src/types/types"
 test("Test balance greater",()=>{
     const user: User ={
         name:"Byron",
-        balance: 100
+        balance: 150
     }
     const result = shopping(user,40)
     expect(result).toEqual({
@@ -15,9 +15,9 @@ test("Test balance greater",()=>{
 test("Test equal balance",()=>{
     const user: User ={
         name:"Byron",
-        balance: 100
+        balance: 110
     }
-    const result = shopping(user,100)
+    const result = shopping(user,110)
     expect(result).toEqual({
         name: "Byron",
         balance: 0
@@ -28,6 +28,6 @@ test("Test lower balance",()=>{
         name:"Byron",
         balance: 100
     }
-    const result = shopping(user,110)
+    const result = shopping(user,200)
     expect(result).not.toBeDefined();
 })
